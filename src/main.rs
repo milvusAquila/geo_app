a6b4278cf502bef6de23424673145d678c49d957
Milvus01 <cotcos.tansin@gmail.com>
2023-02-18 19:47:27 +0100

    version stable avec taille et couleur sur texte

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@
 
 // mod define_frames;
 // use crate::define_frames::*;
-
 use eframe::{self, App, Frame};
-use egui::{self, ScrollArea, Vec2, FontDefinitions, FontData, FontFamily, Color32, Label, Layout, Hyperlink, Separator, RichText, FontId};
-
+use egui::{self, ScrollArea, Vec2, FontFamily, Color32, Label, Layout, Hyperlink, Separator, RichText, FontId};
 
 struct CardData { title: String, desc: String, url: String }
 struct Headlines { articles: Vec<CardData> }
@@ -19,21 +17,18 @@ impl Headlines {
             url: format!("https://examples.com/{a}")
         });
         Headlines { articles: Vec::from_iter(iter) }
-     }
-   fn configure_font(&self) /* -> FontId  */{
-        let mut font_def = FontDefinitions::default();
-        font_def.font_data.insert("LiberationSerif".to_owned(), FontData::from_static(include_bytes!("../fonts/LiberationSerif.ttf")));
-        font_def.families.get_mut(&FontFamily::Proportional).unwrap().insert(0, "LiberationSerif".to_owned());
     }
     fn render_new_cards(&self, ui: &mut eframe::egui::Ui) {
         for a in &self.articles {
             ui.add_space(PADDING);
-            let title = Label::new(format!("Play {}", a.title));
-            ui.add(title);
+            let title = RichText::new(format!("Play {}", a.title))
+                .font(FontId { size: 32.0, family: FontFamily::Proportional })
+                .color(Color32::GREEN);
+            ui.label(title);
             ui.add_space(PADDING);
             let desc = Label::new(&a.desc);
             ui.add(desc);
-            ui.style_mut().visuals.hyperlink_color = CYAN;
+            ui.style_mut().visuals.hyperlink_color = Color32::BROWN;
             ui.add_space(PADDING);
             ui.with_layout(Layout::left_to_right(egui::Align::TOP), |ui| {
                 ui.add(Hyperlink::from_label_and_url("read more", &a.url));
@@ -42,25 +37,17 @@ impl Headlines {
             ui.add(Separator::default());
         }
     }
-
 }
 impl App for Headlines {
     fn update (&mut self, ctx: &egui::Context, _frame: &mut Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
             ScrollArea::new([false, true]).show(ui, |ui: &mut egui::Ui| {
-                // for a in &self.articles {
-                //     ui.label(&a.title);
-                //     ui.label(&a.desc);
-                //     ui.label(&a.url);
-                // }
                 self.render_new_cards(ui)
             })
         });
     }
 }
 const PADDING: f32 = 5.0;
-// const WHITE: Color32 = Color32::from_rgb(255, 255, 255);
-const CYAN: Color32 = Color32::from_rgb(0, 255, 255);
 fn main() {
     let app: Headlines = Headlines::new();
     let mut win_option = eframe::NativeOptions::default();
